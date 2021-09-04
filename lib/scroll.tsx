75953e00443d1d7aecdbfaef2b45f1bb451f4232
Enijar <TheEnijar@gmail.com>
2021-09-04 15:49:25 +0100

    feat: add keyboard shortcut

--- lib/scroll.tsx
+++ lib/scroll.tsx
@@ -219,6 +219,31 @@ export default function Scroll({
     };
   }, []);
 
+  React.useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (!event.metaKey) return;
+      switch (event.key) {
+        case "ArrowRight":
+          stateRef.current.progress.x = 1;
+          break;
+        case "ArrowLeft":
+          stateRef.current.progress.x = 0;
+          break;
+        case "ArrowDown":
+          stateRef.current.progress.y = 1;
+          break;
+        case "ArrowUp":
+          stateRef.current.progress.y = 0;
+          break;
+      }
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, []);
+
   return (
     <ScrollWrapper
       ref={wrapperRef}
