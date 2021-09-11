d05899be68006925e275c3b0f1187b0a063e2a40
Enijar <TheEnijar@gmail.com>
2021-09-11 18:56:22 +0100

    chore: revert demo back to basic example

--- demo/examples/native-comparison/native-comparison.tsx
+++ demo/examples/native-comparison/native-comparison.tsx
@@ -1,133 +1,21 @@
 import React from "react";
-import { useMeasure } from "react-use";
-import { Wrapper } from "./styles";
+import { Box, Wrapper } from "./styles";
 import Scroll from "../../../lib";
-import { ScrollApi } from "../../../lib/scroll";
-import CustomScroll, {
-  Element,
-} from "../../components/custom-scroll/custom-scroll";
 
 export default function NativeComparison() {
-  const scrollRef = React.useRef<ScrollApi>(null);
-
-  React.useEffect(() => {
-    if (scrollRef.current === null) return;
-    let nextFrame: number;
-    (function tick() {
-      nextFrame = requestAnimationFrame(tick);
-      scrollRef.current.update();
-    })();
-    return () => {
-      cancelAnimationFrame(nextFrame);
-    };
-  }, []);
-
-  const boxRefs = React.useRef<HTMLDivElement[]>([]);
-  const [elements, setElements] = React.useState<Element[]>([]);
-
-  const registerElement = React.useCallback((element) => {
-    if (!boxRefs.current.includes(element)) {
-      boxRefs.current.push(element);
-    }
-  }, []);
-
-  const [wrapperRef, wrapperSize] = useMeasure();
-  React.useEffect(() => {
-    const elements: Element[] = [];
-    for (const box of boxRefs.current) {
-      if (!box) continue;
-      elements.push({
-        src: box.getAttribute("src"),
-        x: box.offsetLeft,
-        y: box.offsetTop,
-        width: box.offsetWidth,
-        height: box.offsetHeight,
-        target: box,
-      });
-    }
-    setElements(elements);
-  }, [wrapperSize]);
-
   return (
-    <Wrapper ref={wrapperRef}>
-      <CustomScroll elements={elements}>
+    <Wrapper>
+      <Scroll className="scroll">
         <h2>Custom Scroll 👇</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/882/488/488.jpg?hmac=4Wvu-kbWq01j0ofKNFiFhB3oqOqqLvSDavhumL7LSwk"
-          alt=""
-          className="element"
-        />
-        <h2>0</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/698/488/488.jpg?hmac=7zKlpAzX_tk9SW6FykcLevuNrtqkIrz2U5IvJdFkmgg"
-          alt=""
-          className="element"
-        />
-        <h2>1</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/1005/488/488.jpg?hmac=0_S6G0dxBfIlPVyJvojhiu4FUy5Y48BfjZsz68-cObg"
-          alt=""
-          className="element"
-        />
-        <h2>2</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/409/488/488.jpg?hmac=6_I_83QXwpQMydZsx2TFGI-9TMqHpPSNETzmqg2G9R4"
-          alt=""
-          className="element"
-        />
-        <h2>3</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/264/488/488.jpg?hmac=5_JHukAFn45GzWVJOKyNxscoJ3cCiyfRvq73Jfdyrsk"
-          alt=""
-          className="element"
-        />
-        <h2>4</h2>
-        <img
-          ref={registerElement}
-          src="https://i.picsum.photos/id/698/488/488.jpg?hmac=7zKlpAzX_tk9SW6FykcLevuNrtqkIrz2U5IvJdFkmgg"
-          alt=""
-          className="element"
-        />
-        <h2>5</h2>
-      </CustomScroll>
-
-      <Scroll native style={{ margin: "auto" }}>
+        {Array.from({ length: 10 }).map((_, index) => {
+          return <Box key={index}>{index}</Box>;
+        })}
+      </Scroll>
+      <Scroll native className="scroll">
         <h2>Native Scroll 👇</h2>
-        <img
-          src="https://i.picsum.photos/id/882/488/488.jpg?hmac=4Wvu-kbWq01j0ofKNFiFhB3oqOqqLvSDavhumL7LSwk"
-          alt=""
-        />
-        <h2>0</h2>
-        <img
-          src="https://i.picsum.photos/id/698/488/488.jpg?hmac=7zKlpAzX_tk9SW6FykcLevuNrtqkIrz2U5IvJdFkmgg"
-          alt=""
-        />
-        <h2>1</h2>
-        <img
-          src="https://i.picsum.photos/id/1005/488/488.jpg?hmac=0_S6G0dxBfIlPVyJvojhiu4FUy5Y48BfjZsz68-cObg"
-          alt=""
-        />
-        <h2>2</h2>
-        <img
-          src="https://i.picsum.photos/id/409/488/488.jpg?hmac=6_I_83QXwpQMydZsx2TFGI-9TMqHpPSNETzmqg2G9R4"
-          alt=""
-        />
-        <h2>3</h2>
-        <img
-          src="https://i.picsum.photos/id/264/488/488.jpg?hmac=5_JHukAFn45GzWVJOKyNxscoJ3cCiyfRvq73Jfdyrsk"
-          alt=""
-        />
-        <h2>4</h2>
-        <img
-          src="https://i.picsum.photos/id/698/488/488.jpg?hmac=7zKlpAzX_tk9SW6FykcLevuNrtqkIrz2U5IvJdFkmgg"
-          alt=""
-        />
-        <h2>5</h2>
+        {Array.from({ length: 10 }).map((_, index) => {
+          return <Box key={index}>{index}</Box>;
+        })}
       </Scroll>
     </Wrapper>
   );
--- demo/examples/native-comparison/styles.ts
+++ demo/examples/native-comparison/styles.ts
@@ -7,18 +7,21 @@ export const Wrapper = styled.div`
   h2 {
     text-align: center;
   }
+`;
 
-  img {
-    display: block;
-    width: auto;
-    height: 50vh;
-    aspect-ratio: 1 / 1;
-    margin-bottom: 1em;
-    margin-left: auto;
-    margin-right: auto;
-  }
+export const Box = styled.div`
+  height: 50vh;
+  aspect-ratio: 1 / 1;
+  background-color: crimson;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fefefe;
+  margin-bottom: 1em;
+  margin-left: auto;
+  margin-right: auto;
 
-  .element {
-    opacity: 0;
+  :last-child {
+    margin-bottom: 0;
   }
 `;
