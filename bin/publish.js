8fd42031051b62d348df8a047f22779eb230e117
Enijar <TheEnijar@gmail.com>
2021-09-11 20:09:52 +0100

    chore: update publish script

--- bin/publish.js
+++ bin/publish.js
@@ -2,8 +2,8 @@
 const { execSync } = require("child_process");
 const { version } = require("../package.json");
 
-execSync(`tsc -p tsconfig.lib.json`);
+execSync(`tsc -p tsconfig.lib.json`, { stdio: "inherit" });
 execSync(`git tag -a ${version} -m "release: ${version}"`, {
   stdio: "inherit",
 });
-execSync(`git push origin ${version}`);
+execSync(`git push origin ${version}`, { stdio: "inherit" });
