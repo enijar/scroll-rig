4cc26dec6186c65656c338f96db23626ec1391ff
Enijar <TheEnijar@gmail.com>
2021-09-11 20:05:32 +0100

    chore: add release command

---
+++ bin/release.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+const { version } = require("../package.json");
+
+execSync(`tsc -p tsconfig.lib.json`);
+execSync(`git tag -a ${version} -m "release: ${version}"`, {
+  stdio: "inherit",
+});
+execSync(`git push origin ${version}`);
+execSync(`npm run package`);
+execSync(`npm publish`);
